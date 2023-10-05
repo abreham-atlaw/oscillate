782ddf3f5071b6671aea3db1246ba71a8d3e077e
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-10-05 12:23:08 +0300

    Added Validation

--- src/python/oscillate/training/trainer.py
+++ src/python/oscillate/training/trainer.py
@@ -9,7 +9,9 @@ class TTATrainer:
         self.loss_function = loss_function
         self.optimizer = optimizer
 
-    def train(self, dataloader, epochs: int = 1, progress: bool = False):
+    def train(self, dataloader, val_dataloader=None, epochs: int = 1, progress: bool = False):
+        train_losses = []
+        val_losses = []
         for epoch in range(epochs):
             self.model.train()
             running_loss = 0.0
@@ -23,7 +25,17 @@ class TTATrainer:
                 running_loss += loss.item()
                 if progress:
                     pbar.set_description(f"Epoch {epoch + 1} loss: {running_loss}")
-            print(f"Epoch {epoch + 1} completed, loss: {running_loss / len(dataloader)}")
+            epoch_loss = running_loss / len(dataloader)
+            print(f"Epoch {epoch + 1} completed, loss: {epoch_loss}")
+            train_losses.append(epoch_loss)
+
+            # Validate at the end of each epoch if a validation dataloader is provided
+            if val_dataloader is not None:
+                val_loss = self.validate(val_dataloader)
+                val_losses.append(val_loss)
+                print(f"Validation loss: {val_loss}")
+
+        return train_losses, val_losses
 
     def validate(self, dataloader):
         self.model.eval()
