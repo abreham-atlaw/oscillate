153122073c0e989ccc6b6bd4c298a8c3bf15e75b
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-10-06 12:07:42 +0300

    Added padding_mask

--- src/python/oscillate/model/model/encoder.py
+++ src/python/oscillate/model/model/encoder.py
@@ -18,6 +18,7 @@ class Encoder(nn.Module):
 			mha_heads: int,
 			ff_size: int,
 			*args,
+			pad_token: int = 0,
 			**kwargs
 	):
 		super().__init__(*args, **kwargs)
@@ -27,10 +28,13 @@ class Encoder(nn.Module):
 		self.mha = nn.MultiheadAttention(emb_size, mha_heads)
 		self.add_norm = AddNorm((block_size, emb_size))
 		self.ffn = FeedForwardNetwork(emb_size, ff_size)
+		self.pad_token = pad_token
 
 	def forward(self, X: torch.Tensor):
+		pad_mask = torch.transpose(torch.all(X == torch.zeros((X.shape[2],)), dim=2), 0, 1)
 		y = X + self.pos_encoding(X)
-		attn_output, attn_weights = self.mha(y, y, y)
+		attn_output, attn_weights = self.mha(y, y, y, key_padding_mask=pad_mask)
+		attn_output = attn_output.masked_fill(torch.isnan(attn_output), 0)
 		y = self.add_norm(attn_output, y)
 		ffn_output = self.ffn(y)
 		y = self.add_norm(ffn_output, y)
