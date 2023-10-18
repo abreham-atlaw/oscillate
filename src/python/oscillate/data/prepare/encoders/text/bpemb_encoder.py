b0aac2a3cef3965016d29581b07de95c37306b93
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-10-18 07:49:52 +0300

    Added Logging

--- src/python/oscillate/data/prepare/encoders/text/bpemb_encoder.py
+++ src/python/oscillate/data/prepare/encoders/text/bpemb_encoder.py
@@ -8,11 +8,18 @@ from oscillate.data.prepare.encoders.encoder import Encoder
 
 class BpembEncoder(Encoder):
 
-	def __init__(self, lang: str = "en", emb_size: int = 50):
+	def __init__(self, lang: str = "en", emb_size: int = 50, logging=False):
 		self.__model = BPEmb(lang=lang, dim=emb_size)
 		self.__emb_size = emb_size
+		self.__logging = logging
+
+	def __log(self, text):
+		if not self.__logging:
+			return
+		print(text)
 
 	def encode(self, inputs: typing.Any) -> np.ndarray:
+		self.__log(f"Encoding \"{inputs}\"")
 		tokens = np.array(self.__model.embed(inputs))
 		return tokens
 
